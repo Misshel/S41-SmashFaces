8346863a78113748d99cfe034354e2fb46ce8664
Misshel <jenmish094@gmail.com>
2017-06-10 21:32:23 -0500

    randon de las coders

--- js/components/coders.js
+++ js/components/coders.js
@@ -1,27 +1,20 @@
 'use strict';
 
-
-
 const coderItem = (coder,sede,update,reRender) => {
-  console.log(sede +"img");
-  if(sede == "mexico"){
-    // quien soy
-    console.log(state.mexico.image);
-    let img = $(`<img class="coder" src= "image/${sede}/${coder.imagen}" alt="${coder.name}"/>`);
-  }
-  let img = $(`<img class="coder" src= "image/${sede}/${coder.image}" alt="${coder.name}"/>`);
+  const imagen = coder.image;
+  const name = coder.name;
+  const img = $(`<img class="coder" src= "image/${sede}/${imagen}" alt="${name}"/>`);
 
   return img;
+
 }
-
-
 
 const codersGrid = () =>{
 
   const container = $('<div class="container-fluid"></div>');
 
   const row = $('<div class="row text"></div>');
-  const p = $('<div class="col s12"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div>');
+  const p = $('<div class="col s12"><p>Bienvenidas al juego de Smash Faces tu misión es poder identificar a todas tus compañeras de clase ingresando para ello su nombre. Tienes 5 oportunidades antes de perder un 1 punto y pasar a la siguiente.</p></div>');
   const select = $('<div class="col s6">Elige tu sede:</div>');
   const option = $(' <select id="select" class="" name=""><option value="Peru">Peru</option><option value="Mexico">Mexico</option></select>');
   const puntos = $('<div class="col s6">Puntos:</div>');
@@ -29,16 +22,20 @@ const codersGrid = () =>{
   const rowCoder = $('<div class="row coder"></div>');
   const rowCoderCol = $('<div class="col s12"></div>');
   const contcoder = $('<div class="col s6"></div>');
+  const welcome = $('<div class="col s12">¿Como se llaman nuestras coders?</div>');
 
-  const selecCoder = $('<div class="col s6"></div>');
+  const selecCoder = $('<div class="col s6 selecCoder"></div>');
   const input = $('  <input type="text" name="" value="">');
-  const button = $('<button type="button" name="button">Buscar</button>')
+  const button = $('<button type="button" class="btn" name="button">COMENZAR</button>')
+  const buttonNewCoder = $('<button type="button" class="btn start" name="button">COMPROBAR</button>')
 
 
+  contcoder.append(welcome);
   select.append(option);
 
   selecCoder.append(input);
   selecCoder.append(button);
+  selecCoder.append(buttonNewCoder);
 
   row.append(p);
   row.append(select);
@@ -51,36 +48,46 @@ const codersGrid = () =>{
   container.append(row);
   container.append(rowCoder);
 
-  button.on('click',(e)=>{
+  var click = 0;
+
+  const event = (e)=>{
 
     let sede =  $('select#select').val().toLowerCase();
-    console.log(sede);
-    // if(sede=="Mexico"){
-    //   state.sede== state.mexico;
-    //   alert(state.sede)
-    //   console.log(state.sede);
-    //
-    // }else{
-    //   state.sede== state.peru;
-    // }
+    const sedeCoder = sede;
     if(sede == "mexico"){
-      const findCoder =  filterByName(state.mexico,input.val());
+      let random = randomByName (0,state.mexico.length);
+      sede = state.mexico;
+      reRender(contcoder,random,sede,sedeCoder);
+      click++;
+    } else{
+      let random = randomByName (0,state.peru.length);
+      sede = state.peru;
+      reRender(contcoder,random,sede,sedeCoder);
+      click++;
     }
+    if(click > 0){
+      $("button").addClass( "start");
+      $("button").next().removeClass( "start");
+      $("button").next().addClass( "reStart");
+    }
+  }
 
-    const findCoder =   filterByName(state.peru,input.val());
-    console.log(sede);
-    reRender(contcoder,findCoder,sede);
-    // console.log(    reRender(contcoder,findCoder,sede));
-  });
+  button.on('click',event);
+
+  if( button.css("display:block")){
+    alert("hola");
+    button.css("display:none");
+    buttonNewCoder.css("display:block");
+  }
+  buttonNewCoder.on("click",event);
+
   return container;
 
 }
 
-const reRender = (contcoder, findCoder,sede,update) =>{
+const reRender = (contcoder, random,sede,sedeCoder,update) =>{
   contcoder.empty();
-  findCoder.forEach((coder)=>{
-    console.log(sede);
-    contcoder.append(coderItem(coder,sede,update, _=>{reRender(contcoder, findCoder);}));
-    // contcoder.append(coderItem(coder,update, _=>{reRender(contcoder, findCoder);}));
-  });
+  for (var i = 0; i < 1; i++) {
+    contcoder.append(coderItem(sede[random],sedeCoder,update, _=>{reRender(contcoder, findCoder);}));
+  }
 }
